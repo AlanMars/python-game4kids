bb6b00acc25df595866d5489400336e2539b8b72
Alan Yin <Alan.Yin@autodesk.com>
2018-02-23 15:15:02 +0800

    Add Python Game - mathQuiz - guess number

---
+++ games/mathQuiz.py
@@ -0,0 +1,29 @@
+from os import system
+from random import randint
+
+#this say function is the most important part of kids programming
+#it uses the built in OSX say command to convert text to speech
+def say(something):
+    system('say "%s"' % something)
+
+#how big a number should we guess?
+max_number = 10
+first_line = "Guess a number between 1 and %d" % max_number
+print(first_line)
+say(first_line)
+
+number = randint(1, max_number)
+not_solved = True
+
+#keep looping unil we guess correctly
+while not_solved:
+    answer = eval(input("? "))
+    you_said = "You typed %d" % answer
+    say(you_said)
+    if answer > number:
+        say("The number is lower")
+    elif answer < number:
+        say("The number is higher")
+    else:
+        say("You got it right")
+        not_solved = False
